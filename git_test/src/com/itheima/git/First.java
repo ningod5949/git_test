3366cd2f19d0520468684db87e87b7a56bf2741f
ningod <ningod5949@126.com>
2021-02-09 18:41:44 +0800

    第二次

--- git_test/src/com/itheima/git/First.java
+++ git_test/src/com/itheima/git/First.java
@@ -4,4 +4,8 @@ public class First {
     public static void main(String[] args) {
         System.out.println("hello");
     }
+
+    public void test(){
+        System.out.println("ssss");
+    }
 }
